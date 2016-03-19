ab0ca559c001a047104528e7b4e1fc6b1b1b55e1
Alexander Taylor <programming@Alexanders-MacBook-Pro.local>
2016-03-19 18:38:56 +0000

    Modified ResourcePoint so it can work for objects without a prefab attached.

--- Hackathon 2016/Assets/Scripts/ResourcePoint.cs
+++ Hackathon 2016/Assets/Scripts/ResourcePoint.cs
@@ -10,21 +10,20 @@ public class ResourcePoint : MonoBehaviour {
 	public int health = 5;
 
 	public void Hit() {
-		if (resourceItemPrefab == null) {
-			Debug.LogError("Resource Item Prefab was not provided.");
-			return;
-		}
 		if (health > 0) {
 			health--;
 			return;
 		}
 
-		for (int i = 0; i < amount; i++) {
-			GameObject resource = Instantiate(resourceItemPrefab);
-			Vector3 newPosition = transform.position;
-			newPosition += (new Vector3(Random.value, Random.value, 0) * 5f);
-			resource.transform.position = newPosition;
+		if (resourceItemPrefab != null) {
+			for (int i = 0; i < amount; i++) {
+				GameObject resource = Instantiate(resourceItemPrefab);
+				Vector3 newPosition = transform.position;
+				newPosition += (new Vector3(Random.value, Random.value, 0) * 5f);
+				resource.transform.position = newPosition;
+			}
 		}
+
 		Destroy(gameObject);
 	}
 }
