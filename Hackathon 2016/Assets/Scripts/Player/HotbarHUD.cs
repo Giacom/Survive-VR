66011ad778b791d6aec26b41634232ecb82e472e
Alexander Taylor <programming@Alexanders-MacBook-Pro.local>
2016-03-19 12:35:05 +0000

    Added a simple hotbar.

---
+++ Hackathon 2016/Assets/Scripts/Player/HotbarHUD.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HotbarHUD : MonoBehaviour {
+
+	public HotbarHUDElement[] elements;
+
+	public void Start() {
+		Select(0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < elements.Length; i++) {
+			if (Input.GetKeyDown(KeyCode.Alpha0 + i)) {
+				Select(i - 1);
+			}
+		}
+	}
+
+	void Select(int number) {
+		elements[number].Select();
+		for (int i = 0; i < elements.Length; i++) {
+			if (i == number) {
+				continue;
+			}
+
+			elements[i].DeSelect();
+		}
+	}
+}
---
+++ Hackathon 2016/Assets/Scripts/Player/HotbarHUDElement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HotbarHUDElement : MonoBehaviour {
+
+	public Color selectedColor;
+
+	private Image panel;
+	private Image icon;
+
+	private Color initialColor;
+
+	public void Awake() {
+		panel = GetComponent<Image>();
+		icon = GetComponentInChildren<Image>(true);
+		initialColor = panel.color;
+	}
+
+	public void Select() {
+		panel.color = selectedColor;
+	}
+
+	public void DeSelect() {
+		panel.color = initialColor;
+	}
+}
